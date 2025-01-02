158986753fa595cc47c1f72bf0437974c72ecf4d
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-02 13:32:23 +0530

    Start web project

---
+++ src/main/java/lk/hesh/org/example/hotelbokingmanegement/HotelBokingManegementApplication.java
@@ -0,0 +1,13 @@
+package lk.hesh.org.example.hotelbokingmanegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBokingManegementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBokingManegementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/hesh/org/example/hotelbokingmanegement/HotelBokingManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hesh.org.example.hotelbokingmanegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBokingManegementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

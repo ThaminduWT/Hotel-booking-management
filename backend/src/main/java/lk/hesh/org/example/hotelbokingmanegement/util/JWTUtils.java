d0c5c84cf48a7e5ad8b49879c75c759c7463580e
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-06 01:04:35 +0530

    Fixed error✅

--- backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/util/JWTUtils.java
+++ backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/util/JWTUtils.java
@@ -43,7 +43,7 @@ public class JWTUtils {
 
     public boolean isValidToken(String token, UserDetails userDetails){
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token))
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private boolean isTokenExpired(String token){

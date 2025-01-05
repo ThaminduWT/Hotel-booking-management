cd08125738529ab770260aab83a11df358529d7e
ThaminduWT <weeravardana1234git@gmail.com>
2025-01-06 01:41:02 +0530

    Fixed error✅

--- backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/repo/RoomRepo.java
+++ backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/repo/RoomRepo.java
@@ -3,21 +3,20 @@ package lk.hesh.org.example.hotelbokingmanegement.repo;
 import lk.hesh.org.example.hotelbokingmanegement.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
+import java.nio.file.LinkOption;
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
-public interface RoomRepo extends JpaRepository<Room,Long> {
+public interface RoomRepo extends JpaRepository<Room, Long> {
 
     @Query("SELECT DISTINCT r.roomType FROM Room r")
-    List<String> findDistinctRoomType();
+    List<String> findDistinctRoomTypes();
 
     @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
     List<Room> getAllAvailableRooms();
 
     @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE" +
-   " (bk.checkInDate <= : checkOutDate) AND (bk.checkOutDate >= :checkInDate) )")
+            "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
     List<Room> findAvailableRoomsByDateAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
--- backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/repo/UserRepo.java
+++ backend/src/main/java/lk/hesh/org/example/hotelbokingmanegement/repo/UserRepo.java
@@ -2,11 +2,11 @@ package lk.hesh.org.example.hotelbokingmanegement.repo;
 
 import lk.hesh.org.example.hotelbokingmanegement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.Optional;
 
-@Repository
+
 public interface UserRepo extends JpaRepository<User,Long> {
 
     boolean existsByEmail(String email);
